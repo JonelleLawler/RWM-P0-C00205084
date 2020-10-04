fcd0c345e2707a5a48fee7ea4e6b218314d972a8
JonelleLawler <lawlerjonelle@gmail.com>
2020-10-04 17:24:39 +0000

    running total tests

---
+++ RWM-P0-C00205084/Assets/Tests/RunningTotalTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class RunningTotalTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void RunningTotalTestSimple()
+        {
+            int[] input = { 1, 2, 3, 4, 5, 6 };
+            int[] output = RunningTotal.runTotal(input);
+            int[] expected = { 1, 3, 6, 10, 15, 21};
+
+            // run the following to ensure test fails when it should 
+           //  int[] expected = { 5, 3, 6, 10, 15, 21 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
